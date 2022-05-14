63c164cdfe3bc94f26baeced54aef284e6c5c78d
Ragul <ragul.thangaraju@concertidc.com>
2022-05-14 14:26:06 +0530

    update 3

--- app.js
+++ app.js
@@ -1,7 +1,9 @@
 const express = require("express");
+const chalk = require("chalk");
 const app  = express();
 
 app.get("/", (req, res) => {
+    console.log(chalk.blue('Hello world!'));
     res.json({
         message: "this backend automatically deployed using github actions CICD"
     })
